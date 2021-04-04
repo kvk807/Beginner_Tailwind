900d0c57054113a2ecdfc2f5789dbebc5391fb87
Kirk Vander Kuy <kvk807@gmail.com>
2021-04-04 15:15:09 -0500

    quick styles for deploy

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        teal: colors.teal,
+        orange: colors.orange
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
